9157443435f3cad7823a97b24ceaefe82cdabb77
PattiMelo <patti.lima06@gmail.com>
2023-09-22 15:15:56 -0300

    update e new files

--- scripts/ci/quality-gate-table.sh
+++ scripts/ci/quality-gate-table.sh
@@ -1,10 +1,10 @@
-PRODUCT=$("Stone Benefícios" "Giro Stone" "Acompanhamento de TPV" "Autocred" "Member Get Member")
-
+COVERAGE_REQUIRED= "./scripts/ci/coverage.json"
+PRODUCT=$(echo "$COVERAGE_REQUIRED" | jq -r '.[] | fromjson | .Product')
 
 TABLE_MD="## SMB COVERAGE
 | **Product** | **Branches** | **Class** | **Lines** | **TOTAL** |
 |---------------|-----------|-----------|------------|------------|
-| ${PRODUCT[0]}  | $BRANCH % | $FUNCTIONS % | $LINES % | $TOTAL % |"
+| $PRODUCT  | $BRANCH % | $FUNCTIONS % | $LINES % | $TOTAL % |"
 
 PR_COMMENT="$TABLE_MD"
 gh pr comment $PR_NUMBER --body "$PR_COMMENT"
