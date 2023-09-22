454eb6035249a05b3ec0c7eb053b45a02115f2e8
PattiMelo <patti.lima06@gmail.com>
2023-09-22 14:41:45 -0300

    teste atualização script

---
+++ quality-gate-table.sh
@@ -0,0 +1,9 @@
+PRODUCT=("Stone Benefícios" "Giro Stone" "Acompanhamento de TPV" "Autocred" "Member Get Member")
+
+
+TABLE_MD="## SMB COVERAGE
+| **Product** | **Branches** | **Class** | **Lines** | **TOTAL** |
+|---------------|-----------|-----------|------------|------------|
+| $PRODUCT  | $BRANCH % | $FUNCTIONS % | $LINES % | $TOTAL % |"
+
+printf "sua tabela é: %s\n" $TABLE_MD
