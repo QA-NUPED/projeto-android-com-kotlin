a9f01246e97c5cdc183d282665984486d2ecad5d
PattiMelo <patti.lima06@gmail.com>
2023-09-22 15:57:11 -0300

    teste

--- scripts/ci/quality-gate-table.sh
+++ scripts/ci/quality-gate-table.sh
@@ -1,10 +1,12 @@
-COVERAGE_REQUIRED= "coverage.json"
-PRODUCT=$(echo "$COVERAGE_REQUIRED" | jq -r '.[].Product')
+# COVERAGE_REQUIRED= "coverage.json"
+# PRODUCT=$(echo "$COVERAGE_REQUIRED" | jq -r '.[].Product')
 
 TABLE_MD="## SMB COVERAGE
 | **Product** | **Branches** | **Class** | **Lines** | **TOTAL** |
 |---------------|-----------|-----------|------------|------------|
-| $PRODUCT  | $BRANCH % | $FUNCTIONS % | $LINES % | $TOTAL % |"
+| Stone Benefits  | 40 % | 44 % | 50 % | 50 % |
+| Stone Benefits  | 40 % | 44 % | 50 % | 50 % |
+| Stone Benefits  | 40 % | 44 % | 50 % | 50 % |"
 
 PR_COMMENT="$TABLE_MD"
 gh pr comment $PR_NUMBER --body "$PR_COMMENT"
