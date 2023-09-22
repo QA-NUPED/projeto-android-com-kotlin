7e77550dfdaa5166473363f0ec4a3b40795f9713
PattiMelo <patti.lima06@gmail.com>
2023-09-22 15:18:52 -0300

    teste json

--- scripts/ci/quality-gate-table.sh
+++ scripts/ci/quality-gate-table.sh
@@ -1,4 +1,32 @@
-COVERAGE_REQUIRED= "./scripts/ci/coverage.json"
+COVERAGE_REQUIRED= "{
+   "Product": {
+        "StoneBenefits":{
+            "branches": 20,
+            "class": 30,
+            "lines": 30
+        }, 
+        "GiroStone":{
+            "branches": 20,
+            "class": 30,
+            "lines": 30
+        }, 
+        "AcompanhamentoDeTPV":{
+            "branches": 20,
+            "class": 30,
+            "lines": 30
+        }, 
+        "Autocred":{
+            "branches": 20,
+            "class": 30,
+            "lines": 30
+        }, 
+        "MGM":{
+            "branches": 20,
+            "class": 30,
+            "lines": 30
+        }
+    }
+}"
 PRODUCT=$(echo "$COVERAGE_REQUIRED" | jq -r '.[] | fromjson | .Product')
 
 TABLE_MD="## SMB COVERAGE
