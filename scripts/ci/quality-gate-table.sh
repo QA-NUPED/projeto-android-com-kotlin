84294f8d31296319d507934ccbc64cf30f19e3a4
Gleidison Nascimento | Stone <gleidison.nascimento@stone.com.br>
2023-09-23 00:32:20 -0300

    teste72

--- scripts/ci/quality-gate-table.sh
+++ scripts/ci/quality-gate-table.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-PRODUCT=$(echo "$JSON_CONTENT")
-printf "sua variavel é %s\n" $DATA_PRODUCT
-
-TABLE_MD="## SMB COVERAGE
+# COVERAGE_REQUIRED=“coverage.json”
+# PRODUCT=$(echo “$COVERAGE_REQUIRED” | jq -r ‘.[].Product’)
+TABLE_MD="## Certifique que sua alteração atende o mínimo de cobertura requerido
 | **Product** | **Branches** | **Class** | **Lines** | **TOTAL** |
 |---------------|-----------|-----------|------------|------------|
-| $PRODUCT  | $BRANCH % | $FUNCTIONS % | $LINES % | $TOTAL % |"
-
+| Stone Benefits  | 40 % | 44 % | 50 % | 50 % |
+| Giro Stone  | 40 % | 44 % | 50 % | 50 % |
+| Acompanhamento de TPV  | 40 % | 44 % | 50 % | 50 % |
+| Autocred  | 40 % | 44 % | 50 % | 50 % |"
 PR_COMMENT="$TABLE_MD"
 gh pr comment $PR_NUMBER --body "$PR_COMMENT"
